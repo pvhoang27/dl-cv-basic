61f026d9452b27e8ede73026fe0e6fcec06d119d
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-16 09:21:39 +0700

    update

--- models.py
+++ models.py
@@ -4,7 +4,31 @@ import torch.nn as nn
 class SimpleNeuraNetwork(nn.Module):
   def __init__(self):
     super().__init__()
-    self.fc1 = nn.Linear(in_features=, out_features= )
+    self.flatten = nn.Flatten()
+    self.fc1 = nn.Sequential(
+      nn.Linear(in_features= 3 * 32 *32, out_features= 256 ),
+      nn.ReLU()
+    )
+    self.fc2 = nn.Sequential(
+      nn.Linear(in_features= 256, out_features= 512 ),
+      nn.ReLU()
+    )
+    self.fc3 = nn.Sequential(
+      nn.Linear(in_features= 256, out_features= 1024 ),
+      nn.ReLU()
+    )
+    
 
   def forward(self,x):
-    pass
+    x = self.flatten(x)
+    x = self.fc1(x)
+    x = self.fc2(x)
+    x = self.fc3(x)
+    
+    return x
+
+if __name__ == '__main__':
+  model = SimpleNeuraNetwork()
+  input_data = torch.rand(8, 3, 32, 32)
+  result = model(input_data)
+  print(result)
